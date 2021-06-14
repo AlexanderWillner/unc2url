bc2a3e81dd79daa6ae41be90a28ad1193f673cb2
Alexander Willner <alex@willner.ws>
2021-06-14 21:12:31 +0200

    increased test coverage

--- tests/test_unc2url.py
+++ tests/test_unc2url.py
@@ -2,7 +2,10 @@
 
 """UNC to URL Converter tests."""
 
+import io
+import sys
 import unittest
+import unittest.mock
 
 # import unc2url
 from unc2url import unc2url
@@ -14,6 +17,18 @@ class UNCTestCase(unittest.TestCase):
     UNC = "\\\\example.org\\acme\\data\\Regelmäßige Meetings\\path"
     URL = "smb://example.org/acme/data/Regelmäßige%20Meetings/path"
     FILE = "file://example.org/acme/data/Regelmäßige%20Meetings/path"
+
+    def test_main(self):
+        with self.assertRaises(SystemExit):
+            unc2url.main()
+        with unittest.mock.patch("sys.stdout", new=io.StringIO()) as fake_out:
+            sys.argv = ["", self.UNC]
+            unc2url.main()
+            self.assertEqual(fake_out.getvalue().strip(), self.URL)
+        with unittest.mock.patch("sys.stdout", new=io.StringIO()) as fake_out:
+            sys.argv = ["", "-r", self.URL]
+            unc2url.main()
+            self.assertEqual(fake_out.getvalue().strip(), self.UNC)
 
     def test_unc_to_url(self):
         result = unc2url.unc_to_url(" " + self.UNC + "\n")
